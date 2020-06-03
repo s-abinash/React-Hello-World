defa85a1a3938d3c18321d1cf4a5d682d65a9829
AbinashS <s.abinash891@gmail.com>
2020-06-03 14:13:06 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import React from "react";
-import logo from "./logo.svg";
+import "./notiflix/notiflix.min.css";
 import "./App.css";
+import "./semantic/dist/semantic.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Sample Test</h1>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    name: "Abi",
+    age: 19,
+  };
+  handleClick = (e) => {
+    console.log(this.age);
+    this.setState({
+      age: 20,
+    });
+  };
+  handleCopy = (e) => {
+    console.log(e.target);
+  };
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Sample Test</h1>
+          <p onCopy={this.handleCopy}>
+            My name is {this.state.name} and My age is {this.state.age}
+          </p>
+          <button className="big ui positive button" onClick={this.handleClick}>
+            Change
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
