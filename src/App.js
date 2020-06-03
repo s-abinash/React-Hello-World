d3c64c93d7915cca38fc8f77639a79e0a1253e3e
AbinashS <s.abinash891@gmail.com>
2020-06-03 17:58:41 +0530

    cd

--- src/App.js
+++ src/App.js
@@ -5,29 +5,47 @@ import "./semantic/dist/semantic.min.css";
 
 class App extends React.Component {
   state = {
-    name: "Abi",
-    age: 19,
+    name: "User",
+    age: 0,
+  };
+
+  handleChange = (e) => {
+    document.getElementById("button").disabled = false;
   };
   handleClick = (e) => {
-    console.log(this.age);
     this.setState({
-      age: 20,
+      name: document.getElementById("name").value,
+    });
+    e.target.disabled = true;
+  };
+  handleAge = (e) => {
+    this.setState({
+      age: this.state.age + 1,
     });
   };
-  handleCopy = (e) => {
-    console.log(e.target);
+  handleSubmit = (e) => {
+    this.preventDefault();
   };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1>Sample Test</h1>
-          <p onCopy={this.handleCopy}>
-            My name is {this.state.name} and My age is {this.state.age}
-          </p>
-          <button className="big ui positive button" onClick={this.handleClick}>
-            Change
-          </button>
+          <h1>Form</h1>
+          <p>Welcome {this.state.name}! Your age is {this.state.age}</p>
+          <form className="ui form" onSubmit={this.handleSubmit}>
+            <label>Enter your name:</label>
+            <input
+              type="text"
+              id="name"
+              className="ui text"
+              onChange={this.handleChange}
+            />
+          </form>
+          <br />
+          <div className="inline">
+            <button id="button" className="big ui positive button" onClick={this.handleClick}>Change</button>
+            <button className="big ui blue button" onClick={this.handleAge}>Add Age</button></div>
         </header>
       </div>
     );
